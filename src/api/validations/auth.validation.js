067f11682aa143d8bc9c739a1fd9c46eac9b8c9d
Dario Guarracino <>
2018-07-17 00:42:24 +0200

    Register APIS

--- src/api/validations/auth.validation.js
+++ src/api/validations/auth.validation.js
@@ -6,8 +6,9 @@ module.exports = {
     body: {
       email: Joi.string().email().required(),
       password: Joi.string().required().min(6).max(128),
-      username: Joi.string().required().min(6).max(128),
-      name: Joi.string().required().min(6).max(128)
+      username: Joi.string().min(6).max(128),
+      name: Joi.string().required().min(2).max(128),
+
     },
   },
 
