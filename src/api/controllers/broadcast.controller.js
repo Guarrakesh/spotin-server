245338725bcb8f8241182255b3b40dfe2ff3fae9
Dario Guarracino <>
2018-09-01 21:23:39 +0200

    REST: RESERVATION e BROADCAST API ADMIN: Broadcast CRUD

--- src/api/controllers/broadcast.controller.js
+++ src/api/controllers/broadcast.controller.js
@@ -1,10 +1,13 @@
 const httpStatus = require('http-status');
 const { handler: errorHandler } = require('../middlewares/error');
-
+const ApiError = require('../utils/APIError');
+const { omit } = require('lodash');
+const { Model } = require('mongoose');
 const { Broadcast } = require('../models/broadcast.model');
 const { Business } = require('../models/business.model');
+const { SportEvent } = require('../models/sportevent.model');
 
-
+const { BUSINESS, ADMIN } = require('../middlewares/auth');
 exports.load = async(req, res, next, id) => {
   try {
     const broadcast = await Broadcast.get(id);
@@ -18,15 +21,54 @@ exports.load = async(req, res, next, id) => {
 
 exports.get = (req, res) => res.json(req.locals.broadcast);
 
+exports.create = async (req, res, next) => {
+  try {
 
+
+    const broadcast = new Broadcast(req.body);
+    const {user} = req.locals;
+    // Prima di tutto controllo se l'utente è un admin o un business
+    // Se è un admin, tutto ok
+    // Se è un business, allora devo controllare ulteriormente che req.body.business coincida
+    // o sia incluso nei locali posseduti dall'utente business
+    // Questo per evitare che un'utente business, con la sua access token, possa creare dei broadcast appartenenti
+    // ad altri locali
+
+    let business = {};
+    if (user.role === BUSINESS) {
+      const userBusinesses = await user.businesses();
+      const business = userBusinesses.find(bus => bus._id === req.body.business);
+
+      if (!business);
+      throw new ApiError({message: "You are not authorized to access this business", status: 401});
+    } else {
+      business = await Business.findById(req.body.business);
+    }
+
+    const spots = (await SportEvent.findById(req.body.event)).spots;
+
+    if (!business.spots >= spots)
+      throw new ApiError({message: "You don't have enough spot to buy this event", status: 400});
+    await business.paySpots(spots);
+    const savedBroadcast = broadcast.save();
+
+
+    res.status = httpStatus.CREATED;
+    res.json(savedBroadcast);
+  } catch (error) {
+    next(error);
+  }
+
+}
 exports.list = async (req, res, next) => {
   try {
 
     //TODO: Gestire geolocalizzazione
     let broadcasts;
-    let query = req.query;
-    const { latitude, longitude, radius } = query;
 
+    const filterQuery = omit(req.query, ['latitude', 'longitude','radius', '_end', '_sort', '_order', '_sort']);
+    const {_end = 10, _start = 0, _order = 1, _sort = "_id" } = req.query;
+    const { latitude, longitude, radius } = req.query;
 
     if (latitude && longitude && radius) {
       const business = await Business.findNear(latitude, longitude, radius);
@@ -46,14 +88,16 @@ exports.list = async (req, res, next) => {
       })
 
     } else {
-      let broadcasts = await Broadcast.find(req.query)
-        .populate('business').populate('event')
+      let broadcasts = await Broadcast.paginate(filterQuery, {
+        sort: {[_sort]: _order},
+        offset: parseInt(_start),
+        limit: parseInt(_end - _start),
+        populate: ['business']
+      });
+      res.json(broadcasts);
+
+
     }
-
-
-
-
-
   } catch (error) {
     next(error);
   }
