320ae5f99db999d16addb6703630248ddbca156c
Dario Guarracino <darioguarracino@localhost.localdomain>
2018-10-22 19:51:00 +0200

    fix broadcast model

--- admin/src/containers/admin/resources/broadcasts.js
+++ admin/src/containers/admin/resources/broadcasts.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { List, Datagrid, TextField, ReferenceField, Create, EditButton, ReferenceInput, SimpleForm, TextInput, NumberInput, AutocompleteInput,
   RadioButtonGroupInput, Edit, DisabledInput, SelectInput, BooleanInput} from 'react-admin';
 
+  import { DateTimeInput } from 'react-admin-date-inputs';
+
 
 export const BroadcastList = (props) => (
   <List {...props}>
@@ -45,8 +47,8 @@ export const BroadcastCreate = (props) => (
         {id: "1", name: 'Sconto in percentuale'},
       ]}/>
       <NumberInput source="offer.value"/>
-
-
+      <DateTimeInput source="start_at"  options={{ format: "DD/MM/YYYY, HH:mm:ss"}}/>
+      <DateTimeInput source="end_at"  options={{ format: "DD/MM/YYYY, HH:mm:ss"}}/>
     </SimpleForm>
   </Create>
 );
@@ -71,8 +73,8 @@ export const BroadcastEdit = (props) => (
         {id: "1", name: 'Sconto in percentuale'},
       ]}/>
       <NumberInput source="offer.value"/>
-      <TextInput source="start_at" label="Data di inizio prenotazioni (Default 10 giorni prima inizio evento)"/>
-      <TextInput source="end_et" label="Data fine prenotazione (Default 3 ore dopo inizio evento)"/>
+      <DateTimeInput source="start_at" options={{ format: "DD/MM/YYYY, HH:mm:ss"}}/>
+      <DateTimeInput source="end_at"  options={{ format: "DD/MM/YYYY, HH:mm:ss"}}/>
 
     </SimpleForm>
   </Edit>
