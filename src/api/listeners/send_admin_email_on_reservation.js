3b0b9ca1806c8af808711b2c3f8c73582da24c67
Dario Guarracino <>
2019-04-14 13:04:13 +0200

    Fix mailto field in listeners

--- src/api/listeners/send_admin_email_on_reservation.js
+++ src/api/listeners/send_admin_email_on_reservation.js
@@ -1,17 +1,21 @@
 const mailer = require('.././utils/nodemailer');
 const { email } = require('../../config/vars');
 module.exports = (user, reservation, eventName, businessName) => {
-  const data = {
-    to: "dario.guarracino2@gmail.com",
-    from: email.noreplyMail,
-    template: 'admin-reservation-notify',
-    subject: "Nuova Prenotazione | Spot In",
-    context: {
-      user,
-      reservation,
-      eventName,
-      businessName,
-    }
-  };
-  mailer.sendMail(data);
+  if (process.env.NODE_ENV !== "development") {
+    const data = {
+      to: "info@spotin.it",
+      from: email.noreplyMail,
+      template: 'admin-reservation-notify',
+      subject: "Nuova Prenotazione | Spot In",
+      context: {
+        user,
+        reservation,
+        eventName,
+        businessName,
+      }
+    };
+
+    mailer.sendMail(data);
+  }
+
 };
