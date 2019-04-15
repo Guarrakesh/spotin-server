8cb3125d90960bb9c2f2b95d6ceaf15294df67a3
Dario Guarracino <>
2019-04-15 13:26:04 +0200

    graphql server setup

---
+++ src/graphql/competitions.js
@@ -0,0 +1,36 @@
+const { Competition } = require('../api/models/competition.model');
+
+exports.Competition = `
+
+  extend type Query {
+     getCompetitions(name: String): [Competition]
+  }
+  type NestedSport {
+    id: String!
+    name: String
+  }
+  type Competition {
+    id: ID!
+    name: String
+    sport: NestedSport
+    competitorsHaveLogo: Boolean
+    country: String
+    image_versions: [ImageVersion]
+    slug: String 
+    
+  }
+`;
+
+exports.competitionResolvers = {
+  Query: {
+    async getCompetitions(obj, args, context, info) {
+      const competitions =  await Competition.find({name: { "$regex": args.name, "$options": "i" }});
+      return competitions;
+    }
+  },
+
+  Competition: {
+    sport: async parent => await parent.getSport()
+  },
+
+};
---
+++ src/graphql/schema.js
@@ -0,0 +1,85 @@
+const { merge } = require('lodash');
+const { makeExecutableSchema } = require("graphql-tools");
+
+const Query = `
+  type Query {
+    author(id: Int!): Post
+    
+  }`
+;
+
+const { Competition, competitionResolvers } = require('./competitions');
+const { ImageVersion } = require('./imageversion');
+const { SportEvent, sportEventResolvers } = require('./sportevents');
+
+const types = `
+  
+  type Mutation {
+    # The id of the object.
+    id: ID!
+  }
+
+   interface Node {
+      # The id of the object.
+      id: ID!
+   }
+
+  # Information about pagination in a connection.
+  type PageInfo {
+    # When paginating forwards, are there more items?
+    hasNextPage: Boolean!
+  
+    # When paginating backwards, are there more items?
+    hasPreviousPage: Boolean!
+  
+    # When paginating backwards, the cursor to continue.
+    startCursor: String
+  
+    # When paginating forwards, the cursor to continue.
+    endCursor: String
+  }
+
+  union Searchable = SportEvent | Competition
+  type Query {
+    # Fetches an object given its ID
+    node(
+      # The ID of an object
+      id: ID!
+    ): Node
+    search(input: SearchInput!): [Searchable!]!
+  }
+  
+  input SearchInput {
+    name: String!
+  }
+  
+ 
+
+`;
+
+
+const searchResolvers = {
+  Query: {
+    async search(parent, args, c, i) {
+      const results = [
+        ...(await competitionResolvers.Query.getCompetitions(parent, args.input, c, i)),
+        ...(await sportEventResolvers.Query.getSportEvents(parent, args.input, c, i)),
+      ];
+      return results;
+    },
+  },
+  Searchable: {
+    __resolveType(obj) {
+      if (obj.start_at) { return 'SportEvent' }
+      return 'Competition';
+    }
+  }
+
+};
+
+
+
+module.exports = makeExecutableSchema({
+  typeDefs: [ImageVersion, Competition, SportEvent, types],
+  resolvers: merge(searchResolvers, competitionResolvers, sportEventResolvers),
+});
