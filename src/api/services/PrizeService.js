c3715ca3875ad767ff0f25904172f161067328bc
Dario Guarracino <>
2019-09-16 00:46:39 +0200

    SpotCoinTransaction: dipendenza aggiunta a PrizeSerivce. Il priceclaim ha bisogno dell'id della transazione in modo da completarla una volta assegnato il premio

--- src/api/services/PrizeService.js
+++ src/api/services/PrizeService.js
@@ -2,8 +2,8 @@ const PubSub = require('pubsub-js');
 const BaseMongoService = require('./BaseMongoService');
 const { Prize } = require('../models/prize.model');
 const { PrizeClaim, StatusCodes } = require('../models/prizeclaim.model');
-const User = require('../models/user.model');
 const PRIZE_CLAIMED = 'PRIZE_CLAIMED';
+const SpotCoinTransactionService = require('./SpotCoinTransactionService');
 class PrizeService extends BaseMongoService {
   constructor() {
     super(Prize);
@@ -17,19 +17,18 @@ class PrizeService extends BaseMongoService {
    * @returns {Promise<PrizeClaim>}
    */
   async claim(user, prize, meta) {
-
-    const prizeClaim = await PrizeClaim.create({
+    const prizeClaim = new PrizeClaim({
       userId: user.id,
       prizeId: prize.id,
+      transactionId: transaction.id,
       // Se il premio è immediato, completo subito la richiesta
       status: prize.grantingTime === 0 ? StatusCodes.STATUS_COMPLETED : StatusCodes.STATUS_PENDING,
       requestIP: meta.requestIP,
       requestDevice: meta.requestDevice
     });
-    await User.findOneAndUpdate({ _id: user.id }, {
-      $inc: { spotCoins: prize.cost }
-    });
-
+    const transaction = await SpotCoinTransactionService.registerPrizeClaimTransaction({ user, prize, prizeClaim });
+    prizeClaim.transactionId = transaction.id;
+    await prizeClaim.save();
     PubSub.publish(PRIZE_CLAIMED, { prize, prizeClaim, user } );
     return result;
   }
